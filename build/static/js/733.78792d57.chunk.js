ac5d172e4468ab99984ef9f59cfef89b6895cd1b
“HaMinhLong” <haminhlong3@gmail.com>
2023-03-08 09:40:31 +0700

    add build

---
+++ build/static/js/733.78792d57.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkprotal_web=self.webpackChunkprotal_web||[]).push([[733],{64733:function(n,e,r){r.r(e);r(72791);var t=r(64554),c=r(94667),s=r(80184);e.default=function(){return(0,s.jsx)(c.Z,{content:!1,children:(0,s.jsx)(t.Z,{sx:{padding:2},children:(0,s.jsx)("p",{children:"Th\u1ed1ng k\xea"})})})}}}]);
+//# sourceMappingURL=733.78792d57.chunk.js.map
